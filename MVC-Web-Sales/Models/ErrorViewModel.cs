f87ca140a0eb896c3fb9c7a92c73cc267380135e
Ryoji Kitano <ryojikitanodev@gmail.com>
2020-02-03 13:22:35 -0300

    Returning custom error page

--- MVC-Web-Sales/Models/ErrorViewModel.cs
+++ MVC-Web-Sales/Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace MVC_Web_Sales.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
